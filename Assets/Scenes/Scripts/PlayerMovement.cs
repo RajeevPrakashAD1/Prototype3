0ffc5f91c3bddab8b78c51c1235a7f52b155be47
Rajeev Prakash <prakashrajeev805@gmail.com>
2024-02-09 14:43:40 +0530

    new changes

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; // Adjust the player's movement speed as needed
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Read input from keyboard axes (WASD or arrow keys)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate movement direction based on input
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        // Apply movement to the player's Rigidbody
+        rb.velocity = movement * moveSpeed;
     }
 }
